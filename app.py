34e86cee5c289882b59d07b00abf6ba5f265a489
Cecilia MoSze Tham <68426037+aejade@users.noreply.github.com>
2024-05-31 12:53:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ st.title('Herbie Sensor Readings')
 st.subheader('Welcome to the sensor data dashboard')
 st.write('Here you can see the latest sensor readings from the Herbie project.')
 
-# Placeholder for metrics
-metrics_placeholder = st.empty()
-
 # Placeholder for line chart
 line_chart_placeholder = st.empty()
+
+# Placeholder for metrics
+metrics_placeholder = st.empty()
 
 # Path to JSON key file
 SERVICE_ACCOUNT_FILE = 'herbie_key.json'
@@ -83,10 +83,6 @@ fig_realtime = px.line(prev_data.tail(2000), x=prev_data.index, y=['Light', 'Wat
 # Display line chart
 line_chart_placeholder.plotly_chart(fig_realtime, use_container_width=True)
 
-# Create metrics placeholders
-metrics_labels = ["Light Change", "Water Change", "Soil Moisture Change", "Temperature Change", "Humidity Change"]
-metrics_values = [0] * len(metrics_labels)
-
 # Update metrics and line chart
 while True:
     # Fetch real-time data
@@ -96,9 +92,17 @@ while True:
     differences = calculate_differences(prev_data, new_data)
 
     # Update metrics showing differences
-    for i, label in enumerate(metrics_labels):
-        metrics_values[i] = differences.iloc[-1][label.split()[0]]  # Extract sensor name from label
-        metrics_placeholder.metric(label=label, value=metrics_values[i])
+    light_change = differences['Light'].iloc[-1]
+    water_change = differences['Water'].iloc[-1]
+    soil_moisture_change = differences['Moist'].iloc[-1]
+    temperature_change = differences['Temp'].iloc[-1]
+    humidity_change = differences['Humid'].iloc[-1]
+
+    metrics_placeholder.metric("Light Change", value=light_change)
+    metrics_placeholder.metric("Water Change", value=water_change)
+    metrics_placeholder.metric("Soil Moisture Change", value=soil_moisture_change)
+    metrics_placeholder.metric("Temperature Change", value=temperature_change)
+    metrics_placeholder.metric("Humidity Change", value=humidity_change)
 
     # Update line chart
     fig_realtime = px.line(new_data.tail(2000), x=new_data.index, y=['Light', 'Water', 'Moist', 'Temp', 'Humid'],
